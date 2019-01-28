2f74d9d082ba553fbe3c747217d7ea2ae6e1cd79
adrioro <artistsvillage@me.com>
2019-01-28 10:31:42 -0500

    Fourth commit

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ $("select")
         $("div").text(str);
     })
     .change();
+
+$('textarea').keypress(function (a) {
+    //'keyCode' is the right code.
+    var keycode = (a.keyCode ? a.keyCode : a.which); //'which' tells us whay key is the current entered key.
+    alert(keycode);
+});
